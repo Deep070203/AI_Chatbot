48df2a8d7c13a24c34c067cdb23fcb4d2c8ed7ae
Deep070203 <shahdeep855@gmail.com>
2024-09-26 11:54:42 -0400

    Added some code

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,7 @@
 # Import necessary modules
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.llms import Ollama
+from langchain.schema import(SystemMessage, HumanMessage, AIMessage)
 import streamlit as st
 
 # Set up the Streamlit framework
@@ -29,10 +30,31 @@ prompt=ChatPromptTemplate.from_messages(
         ("user","Question:{question}")
     ]
 )
+clear_button = st.sidebar.button("Clear Conversation", key="clear")
+if clear_button or "messages" not in st.session_state:
+    st.session_state.messages = [
+      SystemMessage(
+        content="you are a helpful AI assistant. Reply your answer in markdown format."
+      )
+    ]
 
 # Create a chain that combines the prompt and the Ollama model
 chain=prompt|llm
 
 # Invoke the chain with the input text and display the output
 if input_text:
-    st.write(chain.invoke({"question":input_text}))
+    st.session_state.messages.append(HumanMessage(content=input_text))
+    with st.spinner("Bot is typing ..."):
+      answer = chain.invoke({"question":input_text})
+      print(answer)
+    st.session_state.messages.append(AIMessage(content=answer))
+    # st.write(chain.invoke({"question":input_text}))
+
+messages = st.session_state.get("messages", [])
+for message in messages:
+    if isinstance(message, AIMessage):
+      with st.chat_message("assistant"):
+        st.markdown(message.content)
+    elif isinstance(message, HumanMessage):
+      with st.chat_message("user"):
+        st.markdown(message.content)
